4ec05b13e51d0b373700b60091cf565632c5967b
Archishmaan74 <archishmaan.upa4@gmail.com>
2025-07-07 13:21:50 +0530

    feature/added button for generating new passwords

--- passwordGenerator_frontend/src/App.jsx
+++ passwordGenerator_frontend/src/App.jsx
@@ -64,6 +64,13 @@ function App() {
         </button>
       </div>
 
+      <button
+        className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 ml-32 font-semibold transition-colors duration-300 rounded-full"
+        onClick={passwordGenerator}
+      >
+        New Password
+      </button>
+
       <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4 text-sm text-white">
         <div className="flex flex-col sm:flex-row sm:items-center gap-2 w-full">
           <input
